e2d5e562089ecf6fd3fdea1bd96a07a410efcded
viswa2811 <59775304+viswa2811@users.noreply.github.com>
2020-04-28 01:49:19 +0530

    Add files via upload

---
+++ f1.jsx
@@ -0,0 +1,14 @@
+<html>
+<head>
+<script type="text/javascript">
+function sayHello()
+{
+    alert("hello");
+}
+</script>
+</head>
+<body>
+click here for result
+<input type="button" onclick="sayHello()" value="say Hello" />
+</body>
+</html>
